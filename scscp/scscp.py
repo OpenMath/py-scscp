6c66bec9cf2b43fefbf6ae9897c625062bfbac61
Luca De Feo <defeo@pippin>
2016-10-10 17:47:10 +0200

    Implemented SCSCP2 Content Dictionary and added some tests

--- scscp/scscp.py
+++ scscp/scscp.py
@@ -1,5 +1,6 @@
 import uuid
 import openmath.openmath as om
+import itertools
 
 class SCSCPError(RuntimeError):
     pass
@@ -65,8 +66,76 @@ def procedure_completed(id, data, **info):
     return _procedure_end('procedure_completed', id, data, **info)
 
 def procedure_terminated(id, error, msg=None, **info):
-    if errors['error_' + error] and msg is None:
-        raise RuntimeError('Must give an error message')
+    if not isinstance(error, om.OMError):
+        if errors['error_' + error] and msg is None:
+            raise RuntimeError('Must give an error message')
+        error = om.OMError(om.OMSymbol('error_' + error, cd='scscp1'), om.OMString(msg))
+    return  _procedure_end('procedure_terminated', id, error, **info)
+
+
+### SCSCP2 content dictionary
+
+def _apply(cmd, data):
+    return om.OMApplication(om.OMSymbol(cmd, cd='scscp2'), data)
+
+def symbol_set(symbols, cdnames=['scscp1', 'scscp2'], cdurls=[], groupnames=[], groupurls=[]):
+    aggregations = (
+        (cdnames, om.OMSymbol('CDName', cd='meta')),
+        (cdurls, om.OMSymbol('CDURL', cd='meta')),
+        (groupnames, om.OMSymbol('CDGroupName', cd='metagrp')),
+        (groupurls, om.OMSymbol('CDGroupURL', cd='metagrp')),
+    )
+    
+    return _apply('symbol_set', symbols
+                      + sum([[om.OMApplication(symbol, om.OMString(x)) for x in list]
+                                 for (list, symbol) in aggregations], []))
+
+def get_allowed_heads():
+    return _apply('get_allowed_heads', [])
+
+def is_allowed_head(name, cd):
+    return  _apply('is_allowed_head', [om.OMSymbol(name, cd)])
+
+def store(data, peristent=False):
+    return _apply('store_' + ('persistent' if persistent else 'session'), [data])
+
+def retrieve(url):
+    return _apply('retrieve', [om.OMReference(url)])
+
+def unbind(url):
+    return _apply('unbind', [om.OMReference(url)])
+
+def get_transient_cd(name):
+    return _apply('get_transient_cd', [om.OMApply(
+        om.OMSymbol('CDName', cd='meta'), [om.OMString(name)]
+    )])
+
+def get_signature(name, cd):
+    return  _apply('get_signature', [om.OMSymbol(name, cd)])
+
+def signature(name, cd, symbol_sets=None, min=None, max=None):
+    if isinstance(symbol_sets, list):
+        n = len(symbol_sets)
+        if min is None:
+            min = om.OMInteger(n)
+        else:
+            assert min <= n, "Too few parameters"
+            min = om.OMInteger(min)
+        assert max is None or max == n, "Too many parameters"
+        max = om.OMInteger(n)
+        symbol_sets = om.OMApplication(om.OMSymbol('list', cd='list1'), symbol_sets)
     else:
-        data = om.OMError(om.OMSymbol('error_' + error, cd='scscp1'), om.OMString(msg))
-        return  _procedure_end('procedure_terminated', id, data, **info)
+        min = om.OMInteger(0 if min is None else min)
+        max = om.OMSymbol('infinity', cd='nums1') if max is None else om.OMInteger(max)
+        if symbol_sets is None:
+            symbol_sets = om.OMSymbol('symbol_set_all', cd='scscp2')
+    return _apply('signature', [om.OMSymbol(name, cd), min, max, symbol_sets])
+
+def get_service_description():
+    return _apply('get_service_description', [])
+
+def service_description(*desc):
+    return _apply('service_description', [om.OMString(d) for d in desc])
+
+def no_such_transient_cd(cd):
+    return om.OMError(om.OMSymbol('no_such_transient_cd', cd='scscp2'), om.OMString(cd))
---
+++ tests/test_scscp_cd.py
@@ -0,0 +1,25 @@
+import unittest
+from scscp import scscp
+from openmath.openmath import *
+
+class TestConnInit(unittest.TestCase):
+    def test_service_description(self):
+        self.assertEqual(scscp.get_service_description(),
+                             OMApplication(
+                                 OMSymbol('get_service_description', 'scscp2', id=None, cdbase=None),
+                                 [], id=None, cdbase=None))
+
+        self.assertEqual(scscp.service_description('Hello', 'world'),
+                             OMApplication(
+                                 OMSymbol('service_description', 'scscp2', id=None, cdbase=None),
+                                 [OMString('Hello', id=None), OMString('world', id=None)],
+                                 id=None, cdbase=None))
+
+    def test_procedure(self):
+        self.assertEqual(scscp.procedure_call(scscp.get_service_description(), id='myid'),
+                             OMObject( OMAttribution( OMAttributionPairs(
+                                 [(OMSymbol('call_id', 'scscp1',), OMString('myid'))]),
+                                 OMApplication(
+                                     OMSymbol('procedure_call', 'scscp1'),
+                                     [OMApplication(
+                                         OMSymbol('get_service_description', 'scscp2'), [])]))))
