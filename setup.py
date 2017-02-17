ec1753bcfc6e9ff3ad8be673f58840ea4b0d8a91
Luca De Feo <defeo@pippin>
2017-02-17 11:40:00 +0100

    Bumped version 0.1.0

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='scscp',
-    version='0.1.0a1',
+    version='0.1.0',
     description='Implementation of the SCSCP protocol',
     url='https://github.com/OpenMath/py-scscp',
     author='Luca De Feo',
     license='MIT',
     classifiers=[
-        'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
