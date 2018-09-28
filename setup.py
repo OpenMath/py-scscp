227295d88241c6c245980c076c5d594efbda41e1
Tom Wiesing <tkw01536@gmail.com>
2018-09-28 12:46:19 +0200

    Version bump to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='scscp',
-    version='0.1.1',
+    version='0.2.0',
     description='Implementation of the SCSCP protocol',
     url='https://github.com/OpenMath/py-scscp',
     author='Luca De Feo',
